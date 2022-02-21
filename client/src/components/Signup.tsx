32a4b3a64f0971279a11412638e8ef3440afd0c6
kerusu-1984 <kerusu1017@yahoo.co.jp>
2022-02-21 14:43:53 +0900

    use react-hook-form without signup API

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,58 +1,73 @@
 import React, { useState } from "react";
-import styles from "../styles/Signup.module.css"
-import Router from "next/router"
+import styles from "../styles/Signup.module.css";
+import Router from "next/router";
 import { BackendService } from "../backend/BackendService";
-import { useInput } from "../hooks/useInput";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export const Signup:React.FC = () => {
-    const [formError,setFormError] = useState("");
-    const [email,EmailAttributes] = useInput("");
-    const [userName,UserNameAttributes] = useInput("");
-    const [password,PasswordAttributes] = useInput("");
+type Inputs = {
+  email: string;
+  name: string;
+  password: string;
+};
 
-    const signup: React.FormEventHandler<HTMLFormElement> = async (event) => {
-      event.preventDefault();  
-        try {
-          await BackendService.signup(userName, password, email);
+export const Signup: React.FC = () => {
+  const [formError, setFormError] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-        } catch (error) {
-          if (error.status === 409) {
-            setFormError("同じメールアドレスが登録されています");
-            return ;
-          }
-          throw error;
-        }
-
-                
-      Router.push('/');
-
-    };
-
-
-    return(
-        <div className={styles.Signup_content}>
+  return (
+    <div className={styles.Signup_content}>
       <div className={styles.Signup_box}>
         <div className={styles.Signup_title}>
           <h1>ユーザー登録</h1>
           <div className="error">{formError}</div>
         </div>
-        <form className={styles.Signup_form} onSubmit={signup}>
-        <div className={styles.Signup_item}>
-          <div className={styles.Signup_label}>メールアドレス</div>
-          <input type="text" {...EmailAttributes}/>
-        </div>
-        <div className={styles.Signup_item}>
-          <div className={styles.Signup_label}>名前</div>
-          <input type="text" {...UserNameAttributes}/>
-        </div>
-        <div className={styles.Signup_item}>
-          <div className={styles.Signup_label}>パスワード</div>
-          <input type="password" {...PasswordAttributes}/>
-        </div>
-        <div className={styles.Signup_buttonGroup}>
-          <button type="submit" className={styles.Signup_button}>登録する</button>
-        </div>
+        <form className={styles.Signup_form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.Signup_item}>
+            <div className={styles.Signup_label}>メールアドレス</div>
+            <input type="text" {...register("email", { required: true })} />
+            {errors.email && (
+              <span style={{ color: "red" }}>
+                このフィールドの入力は必須です
+              </span>
+            )}
+          </div>
+          <div className={styles.Signup_item}>
+            <div className={styles.Signup_label}>名前</div>
+            <input type="text" {...register("name", { required: true })} />
+            {errors.name && (
+              <span style={{ color: "red" }}>
+                このフィールドの入力は必須です
+              </span>
+            )}
+          </div>
+          <div className={styles.Signup_item}>
+            <div className={styles.Signup_label}>パスワード</div>
+            <input
+              type="password"
+              {...register("password", { required: true, minLength: 4 })}
+            />
+            {errors.password?.type === "required" && (
+              <span style={{ color: "red" }}>
+                このフィールドの入力は必須です
+              </span>
+            )}
+            {errors.password?.type === "minLength" && (
+              <span style={{ color: "red" }}>
+                パスワードは4文字以上入力してください
+              </span>
+            )}
+          </div>
+          <div className={styles.Signup_buttonGruop}>
+            <button className={styles.Signup_button}>登録する</button>
+          </div>
         </form>
       </div>
     </div>
-    )};
+  );
+};
