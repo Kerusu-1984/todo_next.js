854b269e9d79ecb5a880a9a340e275dde0ee30a1
kerusu-1984 <kerusu1017@yahoo.co.jp>
2022-02-21 13:54:24 +0900

    use react-hook-form without login API

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -5,49 +5,43 @@ import { useInput } from "../hooks/useInput";
 import { useRecoilState } from "recoil" 
 import { nameState,isLoggedInState } from  "./atoms"
 import { BackendService } from "../backend/BackendService";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type Inputs = {
+  email:string;
+  password:string;
+}
+
 
 export  const Login: React.FC = () => {
-    const [email, emailAttributes] = useInput('');
-    const [password, passwordAttributes] = useInput('');
-    const [formError, setFormError] = useState('');
-    const [isLoggedIn,setIsLoggedIn] = useRecoilState(isLoggedInState)
-    const [name,setName] = useRecoilState(nameState)
-
-    const login: React.FormEventHandler<HTMLFormElement> = async (event) => {
-      event.preventDefault();
-        try{
-            const response = await BackendService.login(email,password);
-            setName(response.name)
-            
-        } catch(error){
-            if (error.status === 401) {
-              setFormError("ログインに失敗しました");
-              return;
-            }
-            throw error;
-        }
-        setIsLoggedIn(true)
-        Router.push('/board');
-    }
-
-
-
-    
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log('onSubmit:', data);
+  console.log('watch:', watch('email'));
+  
     return (
-        <div className={styles.Login_content}>
+            <div className={styles.Login_content}>
           <div className={styles.Login_box}>
             <div className={styles.Login_title}>
-              <h1>ログイン</h1>
-              <div className="error">{formError}</div>
+             <h1>ログイン</h1>
+            <div className="error"></div>
             </div>
-            <form className={styles.Login_form} onSubmit={login}>
-            <div className={styles.Login_item}>
+           <form className={styles.Login_form} onSubmit={handleSubmit(onSubmit)}>
+             <div className={styles.Login_item}>
               <div className={styles.Login_label}>メールアドレス</div>
-              <input type="text" {...emailAttributes}/>
+              <input type="text"  {...register("email",{required:true})}/>
+              {errors.email && (
+                <span style={{ color: 'red' }}>このフィールドの入力は必須です</span>
+              )}
             </div>
             <div className={styles.Login_item}>
               <div className={styles.Login_label}>パスワード</div>
-              <input type="password" {...passwordAttributes}/>
+              <input type="password" {...register("password",{required:true,minLength:4})}/>
+              {errors.password?.type === "required" && (
+                <span style={{ color: 'red' }}>このフィールドの入力は必須です</span>
+              )}
+              {errors.password?.type === "minLength" && (
+                  <span style={{ color: 'red' }}>4文字以上入力してください</span>
+              )}
             </div>
             <div className={styles.Login_buttonGruop}>
               <button className={styles.Login_button}>ログインする</button>
